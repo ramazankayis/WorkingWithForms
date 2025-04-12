86b3481a23eb1ab431fe152a4720fb45964d9d85
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2025-04-13 00:36:40 +0300

    using useRef

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,42 @@
+import { useRef, useState } from "react";
+
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+  const email = useRef();
+  const password = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+    const emailIsValid = enteredEmail.includes("@");
+    // email.current.value = ""; // tavsiye edilmez.
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
+    console.log("sending http request....");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p> Please entere a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
