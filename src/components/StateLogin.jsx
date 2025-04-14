2ae1698532e6a88fc92fee4e18adf4a6083760c2
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2025-04-14 23:44:17 +0300

    added   validations  to stateLogin page

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import Input from "./Input";
+import {
+  isEmail,
+  hasMinLength,
+  isNotEmpty,
+  isEqualsToOtherValue,
+} from "../util/validation.js";
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [enteredPassword, setEnteredPassword] = useState("");
@@ -13,10 +19,13 @@ export default function Login() {
     password: false,
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const emailIsInvalid =
+    didEdit.email &&
+    !isEmail(enteredValues.email) &&
+    !isNotEmpty(enteredValues.email);
 
   const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
     event.preventDefault();
